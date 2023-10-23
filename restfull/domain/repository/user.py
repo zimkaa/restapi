0e47c0ce12a7e05357fe21be2522c9f81039b2d4
Anton Zimin <zimkaa87@gmail.com>
2023-10-23 17:23:58 +0300

    added repository
    Signed-off-by: Anton Zimin <zimkaa87@gmail.com>

---
+++ restfull/domain/repository/user.py
@@ -0,0 +1,34 @@
+from abc import ABC
+from abc import abstractmethod
+
+from restfull.domain.entities.user import BaseUser
+from restfull.domain.entities.user import User
+from restfull.domain.types import UserID
+
+
+class UserRepository(ABC):
+    """Data Access Layer for user info"""
+
+    @abstractmethod
+    async def create(self, user: User) -> User:
+        raise NotImplementedError()
+
+    @abstractmethod
+    async def get_by_id(self, user_id: UserID) -> BaseUser | None:
+        raise NotImplementedError()
+
+    @abstractmethod
+    async def get_all(self) -> list[BaseUser]:
+        raise NotImplementedError()
+
+    @abstractmethod
+    async def update(self, user_id: UserID) -> BaseUser:
+        raise NotImplementedError()
+    
+    @abstractmethod
+    async def delete(self, user_id: UserID) -> BaseUser | None:
+        raise NotImplementedError()
+    
+    @abstractmethod
+    async def search_user_by_name(self, name: str) -> BaseUser | None:
+        raise NotImplementedError()
---
+++ restfull/infrastructure/repository/user.py
@@ -0,0 +1,95 @@
+from typing import Any
+
+from loguru import logger
+from sqlalchemy import delete, insert, select, update
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
+
+from restfull.domain.entities.user import BaseUser, User
+
+# from offline.domain.entities.user import BaseUser, TelegramUser
+# from offline.domain.exceptions import TelegramUserAlreadyExists, UserNotFound, UserPhoneAlreadyExists
+from restfull.domain.repository.user import UserRepository
+from restfull.domain.types import UserID
+from restfull.infrastructure.models.user import UserModel
+
+
+def from_model_to_base_user(model: UserModel) -> BaseUser:
+    return BaseUser(
+        id=model.id,
+        name=model.name,
+        last_name=model.last_name,
+        email=model.email if model.email else "",
+        password=model.password if model.password else "",
+    )
+
+
+class UserRepositorySqlalchemy(UserRepository):
+    def __init__(self, database: async_sessionmaker[AsyncSession]):
+        self.database = database
+
+    async def create(self, user: User) -> bool:
+        async with self.database() as session:
+            user_dict = user.to_dict()
+            # TODO: pure SQL
+            query = insert(UserModel).values(**user_dict)
+            try:
+                await session.execute(query)
+            except Exception as exc:
+                logger.error(exc)
+                raise Exception()
+            await session.commit()
+
+        return True
+
+    async def get_by_id(self, user_id: UserID) -> BaseUser | None:
+        async with self.database() as session:
+            # TODO: pure SQL
+            query = select(UserModel).where(UserModel.id == user_id)
+            result = await session.scalars(query)
+
+        user_model = result.one_or_none()
+        if user_model:
+            return from_model_to_base_user(user_model)
+        return None
+
+    async def get_all(self) -> list[dict[str, Any]]:
+        async with self.database() as session:
+            # TODO: pure SQL
+            query = select(UserModel)
+            result = await session.scalars(query)
+
+        return [from_model_to_base_user(user_model).to_dict() for user_model in result.unique().all()]
+
+    async def update(self, user: BaseUser) -> BaseUser:
+        async with self.database() as session:
+            user_dict = user.to_dict()
+            # TODO: pure SQL
+            query = update(UserModel).where(UserModel.id == user.id).values(**user_dict)
+            try:
+                await session.execute(query)
+            except IntegrityError as exc:
+                logger.error(exc)
+                raise Exception()
+            await session.commit()
+
+        return user
+
+    async def delete(self, user_id: UserID) -> BaseUser | None:
+        async with self.database() as session:
+            # TODO: pure SQL
+            query = delete(UserModel).where(UserModel.id == user_id)
+            result = await session.execute(query)
+
+        user_model = result.one_or_none()
+        if user_model:
+            return from_model_to_base_user(user_model)
+        return None
+
+    async def search_user_by_name(self, name: str) -> list[BaseUser]:
+        async with self.database() as session:
+            # TODO: pure SQL
+            query = select(UserModel).where(UserModel.name == name)
+            result = await session.execute(query)
+
+        return result.scalars().all()
