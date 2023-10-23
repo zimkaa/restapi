1d79f573f2212f03c96e0198e12c6ae2ce20970a
Anton Zimin <zimkaa87@gmail.com>
2023-10-23 17:24:46 +0300

    fixed endpoint
    Signed-off-by: Anton Zimin <zimkaa87@gmail.com>

--- restfull/infrastructure/api/endpoints/get_all_users.py
+++ restfull/infrastructure/api/endpoints/get_all_users.py
@@ -1,14 +1,19 @@
 from fastapi import APIRouter
-from fastapi import Response
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import async_sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from restfull.infrastructure.database.sqlalchemy import create_session
+from restfull.infrastructure.repository.user import UserRepositorySqlalchemy
 
 
 router = APIRouter()
 
 
 @router.get("", summary="Get all users")
-async def get_users() -> Response:
-    return Response(
-        content="SUCCESS",
-        status_code=200,
-        media_type="text/plain",
-    )
+async def get_users(
+    db: async_sessionmaker[AsyncSession] = Depends(create_session),
+):
+    dal = UserRepositorySqlalchemy(db)
+    users_dict = await dal.get_all()
+    return users_dict
