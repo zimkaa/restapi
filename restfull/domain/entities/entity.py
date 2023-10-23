1a939c00ed046ceebe1c8b9975cbfb2b81a3ab77
Anton Zimin <zimkaa87@gmail.com>
2023-10-23 13:25:30 +0300

    fix entity and model
    Signed-off-by: Anton Zimin <zimkaa87@gmail.com>

--- restfull/domain/entities/entity.py
+++ restfull/domain/entities/entity.py
@@ -3,4 +3,4 @@ from pydantic import BaseModel
 
 class Entity(BaseModel):
     class Config:
-        orm_mode = True
+        from_attributes = True
--- restfull/domain/entities/user.py
+++ restfull/domain/entities/user.py
@@ -3,10 +3,17 @@ from typing import Any
 from .entity import Entity
 
 
-class BaseUser(Entity):
+class BaseModel(Entity):
     id: int
-    first_name: str
-    last_name: str
 
     def to_dict(self) -> dict[str, Any]:
         return self.model_dump()
+    
+
+class User(Entity):
+    first_name: str
+    last_name: str
+
+
+class BaseUser(BaseModel, User):
+    ...
