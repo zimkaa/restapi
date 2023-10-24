41ee00e2e837cfd2eeddce3d877a3f2f3fa5bb43
Anton Zimin <zimkaa87@gmail.com>
2023-10-24 12:20:15 +0300

    new error message
    Signed-off-by: Anton Zimin <zimkaa87@gmail.com>

--- restfull/infrastructure/api/endpoints/get_user.py
+++ restfull/infrastructure/api/endpoints/get_user.py
@@ -1,21 +1,38 @@
 from fastapi import APIRouter
 from fastapi import Depends
+from fastapi.responses import JSONResponse
 from sqlalchemy.ext.asyncio import async_sessionmaker
 from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
 
 from restfull.domain.entities.user import BaseUser
+from restfull.infrastructure.api.responses.base import ErrorResponse
+from restfull.infrastructure.api.responses.base import OkResponse
 from restfull.infrastructure.database.sqlalchemy import create_session
 from restfull.infrastructure.repository.user import UserRepositorySqlalchemy
-
 
 router = APIRouter()
 
 
-@router.get("/{user_id}", summary="Get user", response_model=BaseUser | None)
+class BaseUserResponse(OkResponse):
+    payload: BaseUser
+
+
+class ErrorUserResponse(ErrorResponse):
+    payload: str = "User not found"
+
+
+@router.get(
+    "/{user_id}",
+    summary="Get user by id",
+    response_model=BaseUserResponse,
+)
 async def get_user(
     user_id: int,
     db: async_sessionmaker[AsyncSession] = Depends(create_session),
 ):
     dal = UserRepositorySqlalchemy(db)
     user = await dal.get_by_id(user_id)
-    return user
+    if user:
+        return BaseUserResponse(payload=user)
+    return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content=ErrorUserResponse().model_dump())
--- tests/test_handlers/test_get_by_id.py
+++ tests/test_handlers/test_get_by_id.py
@@ -8,6 +8,12 @@ def test_get_user_by_id(client: TestClient, default_user: BaseUserWhitPassword, 
     response = client.get(f"{const.API_PREFIX}/{default_user.id}")
     assert response.status_code == 200
     assert len(response.json()) > 0
-    assert response.json()["name"] == default_user.name
-    assert response.json()["last_name"] == default_user.last_name
-    assert response.json()["email"] == default_user.email
+    assert response.json()["payload"]["name"] == default_user.name
+    assert response.json()["payload"]["last_name"] == default_user.last_name
+    assert response.json()["payload"]["email"] == default_user.email
+
+
+def test_fail_get_user_by_id(client: TestClient, default_user: BaseUserWhitPassword):
+    response = client.get(f"{const.API_PREFIX}/{default_user.id}")
+    assert response.status_code == 404
+    assert response.json()["result"] == "error"
