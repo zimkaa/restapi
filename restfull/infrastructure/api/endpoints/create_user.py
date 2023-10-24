33c80a0dbb59fe1fdb42333f624dc105ca334b75
Anton Zimin <zimkaa87@gmail.com>
2023-10-24 12:49:46 +0300

    fix model create
    Signed-off-by: Anton Zimin <zimkaa87@gmail.com>

--- restfull/infrastructure/api/endpoints/create_user.py
+++ restfull/infrastructure/api/endpoints/create_user.py
@@ -1,21 +1,31 @@
 from fastapi import APIRouter
 from fastapi import Depends
+from fastapi.responses import JSONResponse
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.asyncio import async_sessionmaker
+from starlette import status
 
-# from restfull.domain.entities.user import BaseUser
+from restfull.infrastructure.api.responses.base import ErrorResponse
+from restfull.infrastructure.api.responses.base import OkResponse
 from restfull.domain.entities.user import UserWhitPassword
 from restfull.infrastructure.database.sqlalchemy import create_session
 from restfull.infrastructure.repository.user import UserRepositorySqlalchemy
 
+
+class UnknownErrorResponse(ErrorResponse):
+    payload: str = "See logs"
+
+
 router = APIRouter()
 
 
-@router.post("", summary="Create user")
+@router.post("", summary="Create user", response_model=OkResponse)
 async def create(
     user: UserWhitPassword,
     db: async_sessionmaker[AsyncSession] = Depends(create_session),
 ):
     dal = UserRepositorySqlalchemy(db)
     user = await dal.create(user)
-    return user
+    if user:
+        return OkResponse()
+    return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content=UnknownErrorResponse().model_dump())
--- tests/test_handlers/test_create.py
+++ tests/test_handlers/test_create.py
@@ -2,10 +2,11 @@ from fastapi.testclient import TestClient
 
 from restfull.domain import const
 from restfull.domain.entities.user import UserWhitPassword
+from restfull.infrastructure.api.responses.base import OkResponse
 
 
 def test_create_user(client: TestClient, delete_default_user):
     test_user = UserWhitPassword()
     response = client.post(const.API_PREFIX, json=test_user.to_dict())
     assert response.status_code == 200
-    assert response.json() is True
+    assert response.json() == OkResponse().model_dump()
