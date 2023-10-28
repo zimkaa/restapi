c5de03af2a143a8360e41faebf61f037b9e8fc97
Anton Zimin <zimkaa87@gmail.com>
2023-10-28 10:30:49 +0300

    fix
    Signed-off-by: Anton Zimin <zimkaa87@gmail.com>

--- restfull/infrastructure/config/main.py
+++ restfull/infrastructure/config/main.py
@@ -4,7 +4,6 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     app_version: str = "0.1.0"
 
-    # Project settings:
     project_name: str = "restfull"
 
     database_url: str = "sqlite+aiosqlite:///./restfull.db"
